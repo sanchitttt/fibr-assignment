23b6f9cf645dea14df66c108917f99935a60d3e3
Sanchit <sanchittewari222@gmail.com>
2023-07-09 08:53:48 +0530

    added cors

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    BACKEND_ENDPOINT: process.env.NODE_ENV === 'development' ? 'http://localhost:8082' : ''
+    BACKEND_ENDPOINT: process.env.NODE_ENV === 'development' ? 'http://localhost:8082' : 'https://fibrassignmentbackend.vercel.app'
 }
 
 export default config;
